daaf0cae8682744da475a74944dad4c9bf7fb8aa
Josh <joshua222_98@yahoo.com>
2021-05-11 19:07:34 +0800

    operators

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 
 <?php
-echo 'Mono Crisostomo Ibarra';
+# Mono Crisostomo Ibarra #
+
+# Variables #
+
+// $fname = 'Mono';
+// $mname = 'Crisostomo';
+// $lname = 'Ibarra';
+// $age = '19';
+
+// echo "First name: $fname <br>";
+// echo "Middle name: $mname <br>";
+// echo "Last name: $lname";
+
+# Operators #
+
+// $num1 = 10;
+// $num2 = 3;
+// $sum = $num1 + $num2;
+// $dif = $num1 - $num2;
+// $prod = $num1 * $num2;
+// $quo = $num1 / $num2;
+// $rem = $num1 % $num2;
+
+// echo "SUM: $sum <br>";
+// echo "DIFFERENCE: $dif <br>";
+// echo "PRODUCT: $prod <br>";
+// echo "QUOTIENT: $quo <br>";
+// echo "MODULO: $rem";
 ?>
